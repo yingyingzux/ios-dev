4ff02a6b12d5660bf9bc7efd9bc92acf45cd88f9
yzhanghearsay <yzhang@hearsaycorp.com>
2017-09-12 23:08:34 -0700

    add new cocoapods xcode project

---
+++ TestCocoaPods/TestCocoaPods/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  TestCocoaPods
+//
+//  Created by YingYing Zhang on 9/12/17.
+//  Copyright © 2017 Hearsay Systems. All rights reserved.
+//
+
+import UIKit
+import AFNetworking
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
